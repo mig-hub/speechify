9688d4545e7e51059691ae41adc0a143920d3f01
mig <mig@mypeplum.com>
2010-05-14 12:45:05 +0100

    Add a wrapper to make movements easier

--- plugin.speechify.js
+++ plugin.speechify.js
@@ -18,19 +18,15 @@ $('script:last').after("<link rel='stylesheet' type='text/css' href='" + speechi
 				$$.data('speech', this.title);
 				this.title = ''; // Empty the title to disable the default tooltip
 				$$.hover(function(e){ // Rollover
-					$("body").append("<img src='" + spike_file + "' id='speechify_spike' /><p id='speechify'>" + $$.data('speech') + "</p>"); // Add tooltip and spike to page							  							  
-					$("#speechify, #speechify_spike").show();		
+						$("body").append("<div id='speechify_wrapper'><div id='speechify'>" + $$.data('speech') + "</div><img src='" + spike_file + "' id='speechify_spike' /></div>"); // Add tooltip and spike to page							  							  	
 			    },
 					function(){		
-						$("#speechify, #speechify_spike").remove();
+						$("#speechify_wrapper").remove();
 			  });
 				$$.mousemove(function(e){ // Position
-					$("#speechify_spike")
+					$("#speechify_wrapper")
 						.css("bottom",($(window).height() - ($$.offset().top)) + "px")
-						.css("left",(e.pageX - 8) + "px");
-					$("#speechify")
-						.css("bottom",($(window).height() - ($$.offset().top)) + "px")
-						.css("left",(e.pageX - 8 - 7) + "px");
+						.css("left",(e.pageX) + "px");
 				});
 				$$.click(function(e){ // Make it disapear on click to avoid it to stay on some Ajax tricks
 					$$.mouseout();
